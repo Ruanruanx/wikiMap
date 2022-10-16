ab0b40b9a6425770c58ed578d56d7c7de06b68b6
Ruanruanx <zxin7@uwo.ca>
2022-10-15 23:02:37 -0300

    Modified ejs layout

---
+++ db/queries/points.js
@@ -0,0 +1,18 @@
+const db = require('../connection');
+
+const getPoints = () => {
+  return db.query('SELECT * FROM points;')
+    .then(data => {
+      return data.rows;
+    });
+};
+
+const getPointsById = (id) => {
+  return db.query(`SELECT * FROM maps WHERE id = $1`,[id])
+  .then((data) => {
+    return data.rows[0];
+  });
+};
+
+
+module.exports = { getPoints, getPointsById };
